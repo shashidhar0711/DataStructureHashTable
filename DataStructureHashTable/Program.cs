f4423cfa14718c0ce2c74b30ac57d382da85e295
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-09 21:01:45 +0530

    [sasic] Add . To Find Frequency of Words in Larger Paragraph

--- DataStructureHashTable/Program.cs
+++ DataStructureHashTable/Program.cs
@@ -10,8 +10,25 @@ namespace DataStructureHashTable
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Welcome to Hashmap.");
-            /// Create a  reference of MyMapNode
+            Console.WriteLine("Welcome to Hashmap.");
+
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+            /// Split into array of sub strings.
+            string[] para = paragraph.Split(" ");
+
+            /// Create a reference of MyMapNode
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(para.Length);
+            int key = 0;
+
+            /// iterating a paragraph
+            /// adds key and value in to hash.
+            foreach (string words in para)
+            {
+                hash.Add(key, words);
+                key++;
+            }
+            Console.WriteLine("Frequency are :"+hash.GetFrequency("paranoid"));
         }
     }
 }
