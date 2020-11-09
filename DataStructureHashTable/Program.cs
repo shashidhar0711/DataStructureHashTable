65c4e0cd772c43466a2733b311ff58cd5dfbc37b
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-09 20:07:02 +0530

    Add project files.

---
+++ DataStructureHashTable/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataStructureHashTable
+{
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome to Hashmap.");
+            /// Create a  reference of MyMapNode
+        }
+    }
+}
